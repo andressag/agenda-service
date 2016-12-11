93ea657a61544f34ef3cef4f71e11b65886324c9
Andressa Valesi <andressag.klf@gmail.com>
2016-12-11 11:54:07 -0500

    first resource

---
+++ user-resource/src/main/java/com/andressag/agenda/user/resource/UserResource.java
@@ -0,0 +1,17 @@
+package com.andressag.agenda.user.resource;
+
+import com.andressag.agenda.model.User;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import static java.util.Collections.emptyList;
+
+@RestController
+public class UserResource {
+
+    @GetMapping
+    Iterable<User> findAll() {
+        return emptyList();
+    }
+
+}
