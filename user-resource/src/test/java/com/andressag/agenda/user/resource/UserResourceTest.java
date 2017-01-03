1b250e31a53f2447a16797cefbaa0b85d6074c68
Andressa Valesi <andressag.klf@gmail.com>
2017-01-03 15:36:05 -0500

    adding test methods and enum for AgendaError

--- user-resource/src/test/java/com/andressag/agenda/user/resource/UserResourceTest.java
+++ user-resource/src/test/java/com/andressag/agenda/user/resource/UserResourceTest.java
@@ -1,8 +1,10 @@
 package com.andressag.agenda.user.resource;
 
 import com.andressag.agenda.model.Profile;
+import com.andressag.agenda.user.exception.FindUserException;
 import com.andressag.agenda.user.persistence.UserEntity;
 import com.andressag.agenda.user.persistence.UserRepository;
+import org.apache.catalina.servlet4preview.http.HttpServletRequest;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -15,6 +17,7 @@ import java.util.Set;
 import static com.andressag.agenda.model.Profile.ADMIN;
 import static java.util.Collections.singletonList;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Fail.failBecauseExceptionWasNotThrown;
 import static org.mockito.Mockito.when;
 
 @RunWith(MockitoJUnitRunner.class)
@@ -22,6 +25,9 @@ public class UserResourceTest {
 
     @Mock
     private UserRepository repository;
+
+    @Mock
+    private HttpServletRequest request;
 
     private UserResource resource;
 
@@ -43,11 +49,32 @@ public class UserResourceTest {
         when(repository.findAll()).thenReturn(singletonList(entity));
 
         // When
-        final Iterable<UserView> results = this.resource.findAll(null);
+        final Iterable<UserView> results = this.resource.findAll(request);
 
         // Then
         assertThat(results).isNotNull().hasSize(1);
         UserView firstUser = results.iterator().next();
         assertThat(firstUser.getLogin()).isEqualTo("rodrigo");
     }
+
+    @Test
+    public void shouldWrapException() {
+
+        // Given
+        final String expectedIp = "10.0.0.1";
+        when(repository.findAll()).thenThrow(new RuntimeException("mocked"));
+        when(request.getRemoteAddr()).thenReturn(expectedIp);
+
+        // When
+        try {
+            this.resource.findAll(request);
+            failBecauseExceptionWasNotThrown(FindUserException.class);
+        } catch (FindUserException exception) {
+
+            // Then
+            assertThat(exception.getIpAddress()).isEqualTo(expectedIp);
+            assertThat(exception.getErrorTimestamp()).isNotNull();
+        }
+    }
+
 }
